472fb6d482b7eed48db13f4ed3b630d7fa285270
dom UoA <dkee331@aucklanduni.ac.nz>
2025-09-30 11:52:54 +1300

    refactor

---
+++ plot-radio-pulsar.jl
@@ -0,0 +1,96 @@
+using Random, Statistics, StatsBase, Distributions
+using LinearAlgebra
+using IterTools
+using ProgressBars, Plots
+
+include("weights.jl")
+
+Cu = 4  # Underage cost.
+Co = 1  # Overage cost.
+
+newsvendor_loss(order, demand) =
+    sum(Cu * max(demand[i] - order[i], 0) + Co * max(order[i] - demand[i], 0) for i in eachindex(order))
+
+function newsvendor_order(demand, weights)
+    m, T = size(demand)
+    q = Cu / (Cu + Co)
+    
+    return [quantile(demand[i, :], Weights(weights), q) for i in 1:m]
+end
+
+Random.seed!(42)
+
+dimension = 2
+modes = 3
+
+repetitions = 300
+history_length = 50
+
+# Initial demand-distribution parameters. Mixture of axis-aligned normals.
+μ = [i*100 for i in 1:modes]
+σ = 10
+
+# Demand-mode shift-distribution parameters.
+shift_distribution = [MvNormal(zeros(dimension), (10^2) * I) for _ in 1:modes]
+
+demand = [zeros(dimension, history_length) for _ in 1:repetitions]
+final_demand = [[Vector{Float64}(undef, dimension) for _ in 1:1000] for _ in 1:repetitions]
+
+for repetition in 1:repetitions
+
+    μs = [ones(dimension) * μ[i] for i in 1:modes]
+
+    for t in 1:history_length
+        demand[repetition][:, t] = rand(MixtureModel(MvNormal, [(μs[i], Diagonal(fill((σ)^2, dimension))) for i in 1:modes]))
+        
+        for i in eachindex(μs); μs[i] += rand(shift_distribution[1]); end
+
+    end
+
+    for j in 1:length(final_demand[1])
+        final_demand[repetition][j] = 
+            rand(MixtureModel(MvNormal, [(μs[i] + rand(shift_distribution[i]), Diagonal(fill((σ)^2, dimension))) for i in 1:modes]))
+
+    end
+end
+
+
+function parameter_fit(solve_for_weights, weight_parameters, distance_function)
+    costs = [zeros(length(weight_parameters)) for _ in 1:repetitions]
+
+    Threads.@threads for (weight_parameter_index, repetition) in ProgressBar(collect(IterTools.product(eachindex(weight_parameters), 1:repetitions)))
+        demand_samples = demand[repetition]
+        weights = solve_for_weights(eachcol(demand_samples), weight_parameters[weight_parameter_index], distance_function)
+        order = newsvendor_order(demand_samples, weights)
+
+        costs[repetition][weight_parameter_index] = mean([
+            newsvendor_loss(order, final_demand[repetition][i]) for i in eachindex(final_demand[repetition])])
+    
+    end
+
+    minimal_index = argmin(mean(costs))
+    minimal_costs = [costs[repetition][minimal_index] for repetition in 1:repetitions]
+
+    display([
+        round(mean(minimal_costs), digits = 4),
+        round(sem(minimal_costs), digits = 4),
+        round(weight_parameters[minimal_index], digits = 4)
+    ])
+
+    return minimal_costs
+end
+
+parameter_fit(windowing_weights, history_length, 0)
+
+LogRange(start, stop, len) = exp.(LinRange(log(start), log(stop), len))
+
+windowing_costs = parameter_fit(windowing_weights, unique(ceil.(Int, LogRange(1,history_length,30))), 0)
+smoothing_costs = parameter_fit(smoothing_weights, [[0]; LogRange(1e-3, 1, 30)], 0)
+
+using LinearAlgebra
+d(ξ, ζ) = norm(ξ - ζ, 1)
+WPF_costs = parameter_fit(WPF_weights, [[0]; LogRange(1e-3, 1, 30); [Inf]], d)
+
+display(mean(WPF_costs - smoothing_costs) / mean(smoothing_costs)*100)
+display(sign(mean(WPF_costs - smoothing_costs)) * mean(WPF_costs - smoothing_costs) / sem(WPF_costs - smoothing_costs))
+
--- weights.jl
+++ weights.jl
@@ -3,70 +3,65 @@
 using JuMP, MathOptInterface
 using COPT, Ipopt
 
-using LinearAlgebra
-
-#d(i,j,ξ_i,ξ_j) = norm(ξ_i - ξ_j, 1)
 
 WPF_optimizer = optimizer_with_attributes(COPT.ConeOptimizer, "Logging" => 0, "LogToConsole" => 0, "BarIterLimit" => 50)  
 
 """
     WPF_weights
 
-history_of_observations [vector of vectors] : Historical observations. ([1] being the oldest and [end] being the most recent.)
-d [function] : Metric defining distances between observations.
+observations [vector of vectors] : Historical observations. ([1] being the oldest [end] being the most recent.)
 λ [scalar] : Regularisation parameter.
+d [function] : Metric defining distances between observations.
 
 Solve for the terminal probability distribution under the Wasserstein probability flow as represented with a hypographical formulation using the exponential cone. 
-Occasionaly this gets stuck; see catch_WPF_weights for a slower but reliable alternative.
+Occasionaly this gets stuck; see Ipopt_WPF_weights for a slower but reliable alternative.
 """
-function WPF_weights(history_of_observations, λ)
+function WPF_weights(observations, λ, d)
 
     Problem = Model(WPF_optimizer)
 
-    T = length(history_of_observations)
+    T = length(observations)
     
-	distances = zeros((T,T))
-    for i in 1:T
-        for j in 1:T
-            distances[i,j] = d(i,j,history_of_observations[i],history_of_observations[j])
-        end
-    end
+    if λ == Inf; weights = zeros(T); weights .= 1/T; return weights; end
+    if λ == 0; weights = zeros(T); weights[end] = 1; return weights; end
 	
     @variables(Problem, begin
                             1>= p[i=1:T, t=1:2] >=0 # Probability allocated to observation i at time 1, Probability allocated to observation i at time T.
                             1>= p_diag[1:T] >= 0   # Diagonal probabilities.
                             1>= γ[i=1:T, j=1:T] >=0 # Probability transported from observation i to observation j between times i and i+1. 
                                 z[t=1:T] <= 0 # Hypographical-reformulation variables. Upper bound enforces log probability constraints.
+
                         end)
 
     for t in 1:2
         @constraint(Problem, sum(p[i,t] for i in 1:T) == 1) # Initial and terminal probabilities sum to 1.
+
     end
 
     for i in 1:T
         for j in 1:i
             JuMP.fix(γ[i,j], 0; force = true) # Only transport probability from an earlier observation to a later observation (diagonality constraints).
+
         end
     end
 
     for t in 1:T # Conservation of probability flow.
         @constraint(Problem, p[t,1] + sum(γ[i,t] for i in 1:T) == p_diag[t]) # Entering.
         @constraint(Problem, p_diag[t] == p[t,2] + sum(γ[t,j] for j in 1:T)) # Exiting.
-        #@constraint(Problem, p[t,1] + sum(γ[i,t] for i in 1:T) == p[t,2] + sum(γ[t,j] for j in 1:T))
+        
     end
 
     for t in 1:T # Hypographical-reformulation constraints.
         @constraint(Problem, [z[t]; 1; p_diag[t]] in MathOptInterface.ExponentialCone()) # Enforces the constraint z[t] <= log(p_diag[t])  
-        #@constraint(Problem, [z[t]; 1; p[t,1] + sum(γ[i,t] for i in 1:T)] in MathOptInterface.ExponentialCone()) # Enforces the constraint z[t] <= log(p_diag[t])     
+        
     end
 
     @objective(Problem, Max,
-        sum(z[t] for t in 1:T) - λ*sum(distances[i,j]*γ[i,j] for j in 1:T for i in 1:T))
+        sum(z[t] for t in 1:T) - λ*sum(d(observations[i],observations[j])*γ[i,j] for j in 1:T for i in 1:T))
 
     optimize!(Problem)
 
     try #is_solved_and_feasible(Problem) # Awkwardness to deal with "LP termination status 11 is not wrapped by COPT.jl."
-        
         weights = [max(value(p[i, 2]),0) for i in 1:T] # Maximum likelihood terminal probability distribution.
         weights = weights/sum(weights)
 		return weights
@@ -75,183 +70,58 @@ function WPF_weights(history_of_observations, λ)
 
 	end
 
-    return open_source_WPF_weights(history_of_observations, λ) # Use Ipopt Interior-Point Method if COPT Barrier Method fails.
+    return Ipopt_WPF_weights(observations, λ, d) # Use Ipopt Interior-Point Method if COPT Barrier Method fails.
 
 end
 
 
-function open_source_WPF_weights(history_of_observations, λ)
+function Ipopt_WPF_weights(observations, λ, d)
 
     Problem = Model(optimizer_with_attributes(Ipopt.Optimizer, "print_level" => 0))
 
-    T = length(history_of_observations)
-    
-	distances = zeros((T,T))
-    for i in 1:T
-        for j in 1:T
-            distances[i,j] = d(i,j,history_of_observations[i],history_of_observations[j])
-        end
-    end
+    T = length(observations)
 	
     @variables(Problem, begin
                             1>= p[i=1:T, t=1:2] >=0 # Probability allocated to observation i at time 1, Probability allocated to observation i at time T.
                             1>= p_diag[1:T] >= 0   # Diagonal probabilities.
                             1>= γ[i=1:T, j=1:T] >=0 # Probability transported from observation i to observation j between times i and i+1. 
+
                         end)
 
     for t in 1:2
         @constraint(Problem, sum(p[i,t] for i in 1:T) == 1) # Initial and terminal probabilities sum to 1.
+
     end
 
     for i in 1:T
         for j in 1:i
             JuMP.fix(γ[i,j], 0; force = true) # Only transport probability from an earlier observation to a later observation (diagonality constraints).
+
         end
     end
 
     for t in 1:T # Conservation of probability flow.
         @constraint(Problem, p[t,1] + sum(γ[i,t] for i in 1:T) == p_diag[t]) # Entering.
         @constraint(Problem, p_diag[t] == p[t,2] + sum(γ[t,j] for j in 1:T)) # Exiting.
+
     end
 
     @objective(Problem, Max,
         sum(ifelse(p_diag[t] > 0, log(p_diag[t]), -Inf) for t in 1:T) - # Defining log(0) = -Inf seems to result in faster solves here.
-            λ*sum(distances[i,j]*γ[i,j] for j in 1:T for i in 1:T))
+            λ*sum(d(observations[i],observations[j])*γ[i,j] for j in 1:T for i in 1:T))
 
     optimize!(Problem)
 
-    #if is_solved_and_feasible(Problem)
-        weights = [max(value(p[i, 2]),0) for i in 1:T] # Maximum likelihood terminal probability distribution.
-        weights = weights/sum(weights)
+    weights = [max(value(p[i, 2]),0) for i in 1:T] # Maximum likelihood terminal probability distribution.
+    weights = weights/sum(weights)
 	    
-        return weights
-
-    #else
-    #    return 1/T*ones(T)
-
-    #end
+    return weights
 
 end
 
-function test_open_source_WPF_weights(history_of_observations, λ)
+function smoothing_weights(observations, α, d)
 
-    Problem = Model(optimizer_with_attributes(Ipopt.Optimizer, "print_level" => 0))
-
-    T = length(history_of_observations)
-    
-	distances = zeros((T,T))
-    for i in 1:T
-        for j in 1:T
-            distances[i,j] = d(i,j,history_of_observations[i],history_of_observations[j])
-        end
-    end
-	
-    @variables(Problem, begin
-                            1>= w[i=0:T, j=1:T+1] >=0 # Probability allocated to observation i at time 1, Probability allocated to observation i at time T.
-                        end)
-
-    @constraint(Problem, sum(w[0,j] for j in 1:T) == 1)
-
-    for j in 1:T
-        @constraint(Problem, sum(w[i,j] for i in 0:T) == sum(w[j,k] for k in 1:T+1)) # Initial and terminal probabilities sum to 1.
-    end
-
-    for i in 1:T
-        for j in 1:i
-            JuMP.fix(w[i,j], 0; force = true) # Only transport probability from an earlier observation to a later observation (diagonality constraints).
-        end
-    end
-
-    JuMP.fix(w[0,T+1], 0; force = true)
-
-    @objective(Problem, Max,
-        sum(ifelse(sum(w[i,j] for i in 0:T) > 0, log(sum(w[i,j] for i in 0:T)), -Inf) for j in 1:T) - # Defining log(0) = -Inf seems to result in faster solves here.
-            λ*sum(distances[i,j]*w[i,j] for j in 1:T for i in 1:T))
-
-    optimize!(Problem)
-
-    #if is_solved_and_feasible(Problem)
-        weights = [max(value(w[i, T+1]),0) for i in 1:T] # Maximum likelihood terminal probability distribution.
-        weights = weights/sum(weights)
-	    
-        return weights
-
-    #else
-    #    return 1/T*ones(T)
-
-    #end
-
-end
-
-
-#test
-
-
-function solve_for_probability_flow(time_series, λ)
-
-    Problem = Model(optimizer_with_attributes(Ipopt.Optimizer, "print_level" => 0))
-
-    T = length(time_series)
-
-    if true
-        distances = zeros((T,T))
-        for i in 1:T
-            for j in 1:T
-                distances[i,j] = d(i,j,time_series[i],time_series[j])
-            end
-        end
-    end
-
-    @variables(Problem, begin
-                            1 >= p[i=1:T, t=1:T] >=0          # Probability allocated to observation i at time t.
-                            1 >= γ[i=1:T, j=1:T, t=1:T-1] >=0 # Probability transported from observation i to observation j between times t and t+1. 
-                        end)
-
-    for t in 1:T
-        @constraint(Problem, sum([p[i,t] for i in 1:T]) == 1) # Probabilities Sum to 1.
-    end
-
-    for t in 1:T-1
-        for i in 1:T
-            @constraint(Problem, sum([γ[i,j,t] for j in 1:T]) == p[i,t]) # Conservation of probability flow entering.
-        end
-    end
-
-    for t in 1:T-1
-        for j in 1:T
-            @constraint(Problem, p[j,t+1] == sum([γ[i,j,t] for i in 1:T])) # Conservation of probability flow exiting.
-        end
-    end
-
-    @objective(Problem, Max, 
-        sum([ifelse(p[t,t] > 0, log(p[t,t]), -Inf) for t in 1:T]) - 
-            λ*sum([distances[i,j]*γ[i,j,t] for j in 1:T for i in 1:T for t in 1:T-1]))
-
-    optimize!(Problem)
-
-    if true
-        return [max(value(p[i, T]),0) for i in 1:T] # Maximum likelihood terminal probability distribution.
-    else
-        return
-    end
-end
-
-
-
-
-
-
-#display(open_source_WPF_weights([6.13, 7.85, 6.47, 4.91, 5.54, 7.13], 4))#
-
-#display(test_open_source_WPF_weights([6.13, 7.85, 6.47, 4.91, 5.54, 7.13], 4))
-
-
-#display(solve_for_probability_flow([6.13, 7.85, 6.47, 4.91, 5.54, 7.13], 4))
-
-
-function smoothing_weights(history_of_observations, α)
-
-    T = length(history_of_observations)
+    T = length(observations)
 
     if α == 0; weights = zeros(T); weights .= 1/T; return weights; end
 
@@ -263,16 +133,16 @@ function smoothing_weights(history_of_observations, α)
 end
 
 
-function windowing_weights(history_of_observations, window_size)
+function windowing_weights(observations, s, d)
 
-    T = length(history_of_observations)
+    T = length(observations)
 
     weights = zeros(T)
 
-    if window_size >= T
+    if s >= T
         weights .= 1
     else
-        for t in T:-1:T-(window_size-1)
+        for t in T:-1:T-(s-1)
             weights[t] = 1
         end
     end
@@ -281,4 +151,9 @@ function windowing_weights(history_of_observations, window_size)
 
     return weights
 end
+
+
+#using LinearAlgebra
+#d(ξ, ζ) = norm(ξ - ζ, 2)
+#@assert sum(abs.(WPF_weights([6.13, 7.85, 6.47, 4.91, 5.54, 7.13], 4.0, d) - [0.0, 0.275, 0.021, 0.0, 0.325, 0.379])) <= 1e-3
 
